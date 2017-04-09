7225b0ed4a90dabcbc7762dc73558ed043d45867
Erik Johnson <mindovermiles262@gmail.com>
2017-04-08 21:13:06 -0700

    finish #build_tree with RSpec

--- lib/build_tree.rb
+++ lib/build_tree.rb
@@ -1,3 +1,52 @@
 class BuildTree
+    require './lib/node'
 
+    attr_accessor :root_node
+
+    def initialize(array)
+        @root_node = nil
+        build_tree(array)
+    end
+
+
+    private
+
+
+    def build_tree(data)
+        data.each do |value|
+            node = make_node(value)
+            place_node(node)
+        end
+        return data
+    end
+    
+    def make_node(value)
+        node = Node.new
+        node.value = value
+        return node
+    end
+
+    def place_node(node, current_node = @root_node)
+        @root_node ||= node
+        return if node == @root_node
+            
+        if node.value < current_node.value
+            if current_node.left_child == nil
+                current_node.left_child = node
+                node.parent = current_node
+                return node
+            else
+                place_node(node, current_node.left_child)
+            end   
+        else
+            if current_node.right_child == nil  
+                current_node.right_child = node
+                node.parent = current_node
+                return node
+            else
+                place_node(node, current_node.right_child)
+            end          
+        end
+    end
+    
 end
--- spec/build_tree_spec.rb
+++ spec/build_tree_spec.rb
@@ -2,15 +2,43 @@ require './lib/node'
 require './lib/build_tree'
 
 describe "Node" do
-    it 'raises error if no value passed' do
-        expect { Node.new }.to raise_error(ArgumentError)
-    end
-
-    it 'initializes with a value' do
-        node = Node.new('abc')
-        expect(node.value).to eql('abc')
+    it 'initializes with correct values' do
+        node = Node.new(4)
+        expect(node.value).to eql(4)
         expect(node.parent).to eql(nil)
         expect(node.left_child).to eql(nil)
         expect(node.right_child).to eql(nil)
     end
+end
+
+describe 'BuildTree' do
+    context 'initialization' do
+        it 'raises error if no array passed' do
+            expect { BuildTree.new }.to raise_error(ArgumentError)
+        end
+    end
+
+    context 'from an array' do
+            before do
+                @tree = BuildTree.new( [1,4,7,9] )
+            end
+
+        it 'works left' do
+            expect(@tree.root_node.left_child).to eql(nil)
+        end
+        
+        it 'works right' do
+            expect(@tree.root_node.right_child.value).to eql(4)
+        end
+
+        it 'tracks parent node' do
+            expect(@tree.root_node.right_child.parent.value).to eql(1)
+        end
+
+        it 'sets root node' do
+            expect(@tree.root_node.value).to eql(1)
+        end
+        
+    end
+        
 end
